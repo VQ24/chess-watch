6041490ea5021ffb0927ce2dfeafece6270569ab
Zlotsin Ivan <izlotsin@exadel.by>
2019-01-28 18:20:23 +0300

    chANGE settings functions and state

---
+++ src/constants/settings.js
@@ -0,0 +1,12 @@
+export const DEFAULT_SETTINGS = {
+  gameTime: 600,
+  turnLimit: {
+    enable: false,
+    limitTime: 180,
+    extractFromGameTime: true,
+  },
+  options: {
+    mode: 'dark',
+    language: 'en',
+  }
+}
--- src/settings/settings.js
+++ src/settings/settings.js
@@ -2,55 +2,79 @@ import React, { Component } from 'react';
 import Button from './input-components/button/button';
 import Checkbox from './input-components/checkbox/checkbox';
 import Input from './input-components/input/input';
+import { DEFAULT_SETTINGS } from '../constants/settings';
 import './settings.css';
 
 class Settings extends Component {
 
   constructor (props) {
     super(props);
+    this.state = DEFAULT_SETTINGS;
+  }
+
+  // enableTurnLimit = () => {
+  //   const limitOptions = Object.assign({}, this.state.turnLimit);
+  //   limitOptions.enable = !limitOptions.enable;
+  //   this.setState({turnLimit: limitOptions});
+  // }
+
+  setGameTime = (time) => {
+    this.setState({
+      gameTime: time
+    })
+  }
+
+  setLimitOptions (optionName, optionValue) {
+    const limitOptions = Object.assign({}, this.state.turnLimit);
+    limitOptions[optionName] = optionValue;
+    this.setState({turnLimit: limitOptions});
   }
 
   render() {
     return (
       <div className='settings-container'>
-        <h1 className='margin-bottom-md'>Game settings</h1>
-        <div className='settings-group margin-bottom-md settings-full-width'>
+        <h1 className='margin-bottom-lg'>Game settings</h1>
+        <div className='settings-group settings-full-width'>
           <Input
             label='Game time'
+            initialValue={this.state.gameTime}
+            fullWidth={true}
             active={true}
-            onChange={()=>{}}
-          />
-          <Input
-            label='Input inactive'
-            active={false}
-            initialValue="test val"
-            style={{width: 10 + 'rem'}}
-            onChange={()=>{}}
-          />
-          <Checkbox
-            label='checked active'
-            checked={true}
-            active={true}
-            onClick={()=>{}}
+            onChange={this.setGameTime}
           />
         </div>
-        <div className='settings-group margin-bottom-lg settings-full-width'>
+        <div className='settings-group settings-full-width'>
           <Checkbox
-            label='checked inactive'
-            checked={true}
-            active={false}
-            onClick={()=>{}}
+            label='Enable turn limit'
+            checked={this.state.turnLimit.enable}
+            active={true}
+            onClick={this.setLimitOptions.bind(this, 'enable', !this.state.turnLimit.enable)}
+          />
+          <Input
+            label='Turn limit time'
+            initialValue={this.state.turnLimit.limitTime}
+            fullWidth={true}
+            active={this.state.turnLimit.enable}
+            onChange={(value) => this.setLimitOptions.call(this, 'limitTime', value)}
           />
           <Checkbox
-            label='unchecked active'
+            label='Extract from game time'
+            checked={this.state.turnLimit.extractFromGameTime}
+            active={this.state.turnLimit.enable}
+            onClick={this.setLimitOptions.bind(this, 'extractFromGameTime', !this.state.turnLimit.extractFromGameTime)}
+          />
+        </div>
+        <div className='settings-group settings-full-width'>
+          <Checkbox
+            label='Light mode'
             checked={false}
             active={true}
             onClick={()=>{}}
           />
           <Checkbox
-            label='unchecked inactive'
+            label='Use Russian language'
             checked={false}
-            active={false}
+            active={true}
             onClick={()=>{}}
           />
         </div>
